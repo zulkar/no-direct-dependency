1e38640d84a805b5e42a749554dcd61cbbc6a967
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2026-04-20 14:31:21 +0200

    initial_commit

---
+++ main/src/main/java/org/github/zulkar/Main.java
@@ -0,0 +1,9 @@
+package org.github.zulkar;
+
+import static org.junit.Assert.assertTrue;
+
+public class Main {
+    public static void main(String[] args) {
+        assertTrue(true);
+    }
+}
---
+++ src/main/java/org/github/zulkar/Main.java
@@ -0,0 +1,8 @@
+package org.github.zulkar;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main(String[] args) {
+    }
+}
